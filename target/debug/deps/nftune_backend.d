ce29d334972e3ce75550e7881dffd09fc993cf0e
MANMOHAN14 <tiger12701@outlook.com>
2025-07-12 09:55:15 +0000

    Fix language detection

--- target/debug/deps/nftune_backend.d
+++ target/debug/deps/nftune_backend.d
@@ -1,6 +1,6 @@
-/home/manmohan/music-hub/target/debug/deps/libnftune_backend.so: src/nftune_backend/src/lib.rs src/nftune_backend/src/types.rs src/nftune_backend/src/storage.rs src/nftune_backend/src/auth.rs src/nftune_backend/src/projects.rs src/nftune_backend/src/nfts.rs
+/home/manmohan/music-hub/target/debug/deps/libnftune_backend.so: src/nftune_backend/src/lib.rs src/nftune_backend/src/types.rs src/nftune_backend/src/storage.rs src/nftune_backend/src/auth.rs src/nftune_backend/src/projects.rs src/nftune_backend/src/nfts.rs src/nftune_backend/src/collaborations.rs
 
-/home/manmohan/music-hub/target/debug/deps/nftune_backend.d: src/nftune_backend/src/lib.rs src/nftune_backend/src/types.rs src/nftune_backend/src/storage.rs src/nftune_backend/src/auth.rs src/nftune_backend/src/projects.rs src/nftune_backend/src/nfts.rs
+/home/manmohan/music-hub/target/debug/deps/nftune_backend.d: src/nftune_backend/src/lib.rs src/nftune_backend/src/types.rs src/nftune_backend/src/storage.rs src/nftune_backend/src/auth.rs src/nftune_backend/src/projects.rs src/nftune_backend/src/nfts.rs src/nftune_backend/src/collaborations.rs
 
 src/nftune_backend/src/lib.rs:
 src/nftune_backend/src/types.rs:
@@ -8,3 +8,4 @@ src/nftune_backend/src/storage.rs:
 src/nftune_backend/src/auth.rs:
 src/nftune_backend/src/projects.rs:
 src/nftune_backend/src/nfts.rs:
+src/nftune_backend/src/collaborations.rs:
